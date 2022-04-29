a476f662d05fe643fde9c64e899c668d146d8281
Chris <chris.ware.dev@gmail.com>
2022-04-28 21:25:14 -0700

    Hero Section  Start

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Hero from '../src/comps/Hero'
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Waddddupppp!!!!!!!</h1>
+    <div>
+      <Hero />
     </div>
   )
 }
---
+++ src/comps/Hero.js
@@ -0,0 +1,11 @@
+import styles from './Hero.module.css'
+
+function Hero() {
+    return (
+        <section className={styles.heroContainer}>
+           <h1>Hero Section</h1>
+        </section>
+    )
+}
+
+export default Hero
