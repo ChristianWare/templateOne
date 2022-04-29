2fa6ce9fe46a0b06940d393fbdf8df98b84a5144
Chris <chris.ware.dev@gmail.com>
2022-04-28 21:58:20 -0700

    Footer, Nav, Layout

---
+++ src/comps/Layout.js
@@ -0,0 +1,22 @@
+import styles from "./Layout.module.css";
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+function Layout({ children }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.background}>
+        <div className={styles.layout}>
+          {/* <ScrollIndicator /> */}
+          {/* <ScrollToTop /> */}
+          <Nav />
+          {children}
+          {/* <PreFooter /> */}
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/comps/Nav.js
@@ -0,0 +1,11 @@
+import styles from './Nav.module.css'
+
+function Nav() {
+    return (
+        <div>
+            Navbar
+        </div>
+    )
+}
+
+export default Nav
