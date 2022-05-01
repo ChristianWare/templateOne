432cfafd29b79746f92f4599a16d371d23d5b633
Chris <chris.ware.dev@gmail.com>
2022-05-01 14:06:59 -0700

    Services MQ's

--- src/comps/Services.js
+++ src/comps/Services.js
@@ -1,29 +1,32 @@
 import styles from "./Services.module.css";
 function Services() {
   return (
-    <div className={styles.services}>
+    <div className={styles.servicesContainer}>
       <div className={styles.servicesBoxes}>
         <div className={styles.bx1}>
-          <h3>Service One</h3>
+          <h3>Service 1</h3>
           <p>
             Services details description filler text will go her in this very
-            spot.
+            spot.Services details description filler text will go her in this
+            very spot.
           </p>
           <h4>READ MORE</h4>
         </div>
         <div className={styles.bx1}>
-          <h3>Service One</h3>
+          <h3>Service 2</h3>
           <p>
             Services details description filler text will go her in this very
-            spot.
+            spot.Services details description filler text will go her in this
+            very spot.
           </p>
           <h4>READ MORE</h4>
         </div>
         <div className={styles.bx1}>
-          <h3>Service One</h3>
+          <h3>Service 3</h3>
           <p>
             Services details description filler text will go her in this very
-            spot.
+            spot.Services details description filler text will go her in this
+            very spot.
           </p>
           <h4>READ MORE</h4>
         </div>
