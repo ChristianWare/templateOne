cf8d14b31f4f6485b3bca0a69f1a40c43ef8a90d
Chris <chris.ware.dev@gmail.com>
2022-05-03 14:09:29 -0700

    Why Choose Us Section Start

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Hero from "../src/comps/Hero";
 import Services from "../src/comps/services";
 import AboutSection from "../src/comps/AboutSection";
 import Misc from "../src/comps/Misc";
+import WhyChooseUs from "../src/comps/WhyChooseUs";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Services />
       <AboutSection />
       <Misc />
+      <WhyChooseUs />
     </div>
   );
 }
---
+++ src/comps/WhyChooseUs.js
@@ -0,0 +1,11 @@
+import styles from "./WhyChooseUs.module.css";
+
+function WhyChooseUs() {
+    return (
+      <div>
+        <h1>WhyChooseUs</h1>
+      </div>
+    );
+}
+
+export default WhyChooseUs
