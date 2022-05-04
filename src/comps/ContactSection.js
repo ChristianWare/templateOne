92a5ec36d048ee50a40cf4d7b1bfaaee978b59fb
Chris <chris.ware.dev@gmail.com>
2022-05-03 23:40:02 -0700

    Contact Form Structure Completed

--- src/comps/ContactSection.js
+++ src/comps/ContactSection.js
@@ -1,4 +1,5 @@
 import styles from "./ContactSection.module.css";
+import Button from "./utils/Button";
 
 function ContactSection() {
   return (
@@ -16,69 +17,30 @@ function ContactSection() {
         </div>
         <div className={styles.right}>
           <div className={styles.contactForm}>
-            <div className={styles.contactTop}>
-              <div className={styles.inputBox}>
-                <input type='text' id='name' required />
+            <h2>Shoot Me A Line</h2>
+            <form className={styles.formBox} onSubmit={(e) => onSubmitForm(e)}>
+              <div className={styles.inputBox50}>
+                <input id='name' type='text' required />
                 <label htmlFor='name'>Name</label>
               </div>
-              <div className={styles.inputBox}>
-                <input type='text' id='Company' required />
-                <label htmlFor='company'>Company</label>
+              <div className={styles.inputBox50}>
+                <input id='email' type='text' required />
+                <label htmlFor='email'>Email Address</label>
               </div>
-              <div className={styles.inputBox}>
-                <input type='text' id='Phone' required />
+              <div className={styles.inputBox50}>
+                <input id='phone' type='text' required />
                 <label htmlFor='phone'>Phone #</label>
               </div>
-              <div className={styles.inputBox}>
-                <input type='text' id='Email' required />
-                <label htmlFor='email'>Email</label>
+              <div className={styles.inputBox100}>
+                <textarea id='message' required></textarea>
+                <label htmlFor='message'>Tell Me About Your Business...</label>
               </div>
-              <div className={styles.inputBox}>
-                <input type='text' id='message' required />
-                <label htmlFor='message'>A Brief description here...</label>
-              </div>
-            </div>
-            <div className={styles.contactBottom}>
-              <h3>Services</h3>
-              <div className={styles.checkBoxes}>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
-                </div>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
-                </div>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
-                </div>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
-                </div>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
-                </div>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
-                </div>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
-                </div>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
-                </div>
-                <div className={styles.check}>
-                  <input type='checkbox' id='option1' name='option1' />
-                  <label htmlFor='option1'>Option1</label>
+              <div className={styles.btnContainer}>
+                <div className={styles.containerBtn}>
+                  <Button text="Send Message" color='tertiary' />
                 </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
