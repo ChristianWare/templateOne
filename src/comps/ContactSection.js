6b353b446021b27dc4cfa865a5a00f2ff279f8b7
Chris <chris.ware.dev@gmail.com>
2022-05-03 22:40:52 -0700

    Contact Section Start

--- src/comps/ContactSection.js
+++ src/comps/ContactSection.js
@@ -1,7 +1,89 @@
 import styles from "./ContactSection.module.css";
 
 function ContactSection() {
-  return <div className={styles.contactSectionContainer}>Contact Section</div>;
+  return (
+    <section className={styles.contactSectionContainer}>
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <h2 className={styles.heading}>
+            Drop Us <br />
+            <span>A Line</span>
+          </h2>
+          <p className={styles.copy}>
+            Here are the best ways to contact us. Feel free to reach out at any
+            time that is good for you.
+          </p>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.contactForm}>
+            <div className={styles.contactTop}>
+              <div className={styles.inputBox}>
+                <input type='text' id='name' required />
+                <label htmlFor='name'>Name</label>
+              </div>
+              <div className={styles.inputBox}>
+                <input type='text' id='Company' required />
+                <label htmlFor='company'>Company</label>
+              </div>
+              <div className={styles.inputBox}>
+                <input type='text' id='Phone' required />
+                <label htmlFor='phone'>Phone #</label>
+              </div>
+              <div className={styles.inputBox}>
+                <input type='text' id='Email' required />
+                <label htmlFor='email'>Email</label>
+              </div>
+              <div className={styles.inputBox}>
+                <input type='text' id='message' required />
+                <label htmlFor='message'>A Brief description here...</label>
+              </div>
+            </div>
+            <div className={styles.contactBottom}>
+              <h3>Services</h3>
+              <div className={styles.checkBoxes}>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+                <div className={styles.check}>
+                  <input type='checkbox' id='option1' name='option1' />
+                  <label htmlFor='option1'>Option1</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ContactSection;
