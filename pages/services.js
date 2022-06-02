298aa854638677543e617379492bf8b4a1168a3a
Chris <chris.ware.dev@gmail.com>
2022-06-02 13:39:16 -0700

    Single Service/Services Page Update

--- pages/services.js
+++ pages/services.js
@@ -4,6 +4,7 @@ import BG from "../public/images/bg.jpg";
 import Image from "next/image";
 import Features from "../src/comps/features/Features";
 import SectionHeader from "../src/comps/utils/sectionHeader/SectionHeader";
+import SingleService from "../src/comps/utils/singleService/SingleService";
 
 function services() {
   return (
@@ -13,30 +14,30 @@ function services() {
         heading='Services Page'
         copy='Here are a list of the services that we provide. You will also find information on the features that come with each service. If you have any questions feel free to reach out to us at any time.'
       />
-      <SectionHeader text='Service 0ne' />
-      <article className={styles.content}>
-        <div className={styles.imgOverlay}></div>
-        <Image
-          src={BG}
-          layout='fill'
-          objectFit='cover'
-          className={styles.img}
-        />
-        <div className={styles.left}>
-          <h2>Service #1</h2>
-          <p>
-            We are a visulal production company, specializing in ariel drone
-            videography and photography. We are a visulal production company, 0
-            specializing in ariel drone videography and photography.
-          </p>
-          <h3>
-            We are a visulal production company, specializing in ariel drone
-            videography.
-          </h3>
-        </div>
-      </article>
+      <SingleService
+        header='Service One'
+        src={BG}
+        angle='left'
+        outlineText='outlineTextLeft'
+        jc='flexEnd'
+        btnDirection='btnRight'
+      />
+      <SingleService
+        header='Service Two'
+        src={BG}
+        angle='right'
+        outlineText='outlineTextRight'
+      />
+      <SingleService
+        header='Service Three'
+        src={BG}
+        angle='left'
+        outlineText='outlineTextLeft'
+        jc='flexEnd'
+        btnDirection='btnRight'
+      />
 
-      <SectionHeader
+      {/* <SectionHeader
         text='Service Two'
         outlineText='outlineTextRight'
         side='right'
@@ -85,7 +86,7 @@ function services() {
           </h3>
         </div>
         <div className={styles.right}></div>
-      </div>
+      </div> */}
       <Features />
     </section>
   );
---
+++ src/comps/utils/singleService/SingleService.js
@@ -0,0 +1,44 @@
+import styles from "./SingleService.module.css";
+import SectionHeader from "../sectionHeader/SectionHeader";
+import Image from "next/image";
+import Button from "../button/Button";
+
+function SingleService({
+  header,
+  src,
+  angle = "",
+  outlineText = "",
+  jc = "",
+  btnDirection = "",
+}) {
+  return (
+    <article>
+      <SectionHeader text={header} side={angle} outlineText={outlineText} />
+      <div className={`${styles.content} ${styles[jc]}`}>
+        <div className={styles.imgOverlay}></div>
+        <Image
+          src={src}
+          layout='fill'
+          objectFit='cover'
+          className={styles.img}
+        />
+        <div className={`${styles.headingCopyContainer} ${styles[angle]}`}>
+          <h2 className={styles.heading}>{header}</h2>
+          <p className={styles.copy} lang='en'>
+            We are a visulal production company, specializing in ariel drone
+            videography and photography. We are a visulal production company,
+            specializing in ariel drone videography and photography. We are a
+            visulal production company, specializing in ariel drone videography
+            and photography.
+          </p>
+        </div>
+        <div className={`${styles.btnContainer} ${styles[btnDirection]}`}>
+          <Button text='call us' color='primary' />
+          <Button text='pricing info' color='primaryOutline2' />
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default SingleService;
