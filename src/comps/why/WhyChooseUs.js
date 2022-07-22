9698f8f97b2f2a1b745080ecf48f27fab2e85c38
ChristianWare <c.p.ware@hotmail.com>
2022-07-22 13:59:40 -0700

    Why Us Updated Content

--- src/comps/why/WhyChooseUs.js
+++ src/comps/why/WhyChooseUs.js
@@ -14,34 +14,30 @@ function WhyChooseUs() {
         <div className={styles.reasonBoxes}>
           <div className={styles.reason}>
             <Plane className={styles.icon} />
-            <h3>Reason 1</h3>
+            <h3>Professional</h3> 
             <p>
-              We are a visulal production company, specializing in ariel drone
-              videography and photography.
+              We are professional. period. We know what it takes to navugate these waters. 
             </p>
           </div>
           <div className={styles.reason}>
             <Plane className={styles.icon} />
-            <h3>Reason 2</h3>
+            <h3>Experienced</h3>
             <p>
-              We are a visulal production company, specializing in ariel drone
-              videography and photography.
+              We have been doing this for a long time. You will be glad you chose us. 
             </p>
           </div>
           <div className={styles.reason}>
             <Plane className={styles.icon} />
-            <h3>Reason 3</h3>
+            <h3>Punctual</h3>
             <p>
-              We are a visulal production company, specializing in ariel drone
-              videography and photography.
+              We deliver all work on time, for every project. We pride ourselves on providing you what you need in a timley manner. 
             </p>
           </div>
           <div className={styles.reason}>
             <Plane className={styles.icon} />
-            <h3>Reason 4</h3>
+            <h3>Passionate</h3>
             <p>
-              We are a visulal production company, specializing in ariel drone
-              videography and photography.
+              Our passion can clearly be seen in the work we put out. We won't fininsh a project unless u it is picture perfect.
             </p>
           </div>
         </div>
