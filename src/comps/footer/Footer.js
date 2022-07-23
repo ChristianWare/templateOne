fd34307e4c4e3c9eed5c4d24a4adb32ab3d87283
ChristianWare <c.p.ware@hotmail.com>
2022-07-22 19:50:15 -0700

    Removed Sitemap

--- src/comps/footer/Footer.js
+++ src/comps/footer/Footer.js
@@ -2,14 +2,14 @@ import styles from "./Footer.module.css";
 import SectionHeader from "../utils/sectionHeader/SectionHeader";
 import NavBarElement from "../utils/navbarElement/NavBarElement";
 import Link from "next/link";
-import ContactSection from '../contactSection/ContactSection'
+import ContactSection from "../contactSection/ContactSection";
 
 function Footer() {
   return (
     <>
-    {/* <ContactSection /> */}
+      {/* <ContactSection /> */}
       <div className={styles.sectionHeaderContaqiner}>
-        <SectionHeader text='call now!' />
+        <SectionHeader text="call now!" />
       </div>
       <footer className={styles.footerContainer}>
         <div className={styles.footerBottom}>
@@ -17,17 +17,7 @@ function Footer() {
           <div className={styles.copyRightContainer}>
             <small>
               Company Name {new Date().getFullYear()} &copy; - All Rights
-              Reserved || Designed and Developed by{" "}
-              <Link href='https://www.fontsandfooters.com/' passHref>
-                <a target='_blank'>Fonts & Footers</a>
-              </Link>{" "}
-              ||{" "}
-              <Link
-                href='https://www.fontsandfooters.com/sitemap-0.xml'
-                passHref
-              >
-                <a target='_blank'>Sitemap</a>
-              </Link>
+              Reserved || Designed and Developed by Chris Ware
             </small>
           </div>
         </div>
